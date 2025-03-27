549dde5e5aadc67bcbc258093bc7a83f8534572a
Ajmat <hunterboyz73770@gmail.com>
2025-03-27 23:16:21 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  apps: [{
-    name: "FlaskApp",
-    script: "gunicorn",
-    args: "main:app -b 0.0.0.0:5000",
-    interpreter: "python3"
-  },{
-    name: "NodeBot",
-    script: "bot.js"
-  }]
-}
+  apps: [
+    {
+      name: "Instagram DM Bot",
+      script: "bot.js",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
