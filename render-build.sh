a630c311cd348abd3daac4914a3c8b68aeb54f59
Ajmat <hunterboyz73770@gmail.com>
2025-03-28 03:48:18 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-playwright install --with-deps
+# Install Playwright dependencies
+apt-get update && apt-get install -y libnss3 libatk-bridge2.0-0 libxcomposite1 \
+    libxcursor1 libxdamage1 libxi6 libgbm1 libasound2 libxrandr2
+
+# Install Playwright
+pip install playwright
+playwright install
