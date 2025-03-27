012fbb0124db63a41c984c6d702de03ff9c2542a
Ajmat <hunterboyz73770@gmail.com>
2025-03-27 21:29:09 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,12 @@ module.exports = {
   apps: [
     {
       name: "FlaskApp",
-      script: "gunicorn",
-      args: "main:app --bind 0.0.0.0:5000",
-      interpreter: "python3",
+      script: "gunicorn main:app -b 0.0.0.0:5000",
+      interpreter: "python3"
     },
     {
       name: "NodeBot",
-      script: "bot.js",
-      interpreter: "node",
-      env: {
-        CHROME_BIN: "/usr/bin/google-chrome-stable"
-      }
+      script: "bot.js"
     }
   ]
-};
+}
