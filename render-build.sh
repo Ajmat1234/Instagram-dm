744d9ea55436d29eaa6febeb3802496c440c9990
Ajmat <hunterboyz73770@gmail.com>
2025-03-28 10:35:28 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y libgtk-4-dev libgraphene-1.0-0 \
-    libnss3 libatk-bridge2.0-0 libxcomposite1 libxcursor1 libxdamage1 libxi6 libgbm1 \
-    libasound2 libxrandr2 fonts-liberation libappindicator3-1 libdbusmenu-glib4 \
-    libdbusmenu-gtk3-4 libx11-xcb1 libxtst6 xdg-utils libpangocairo-1.0-0 \
-    libatk1.0-0 libcups2
